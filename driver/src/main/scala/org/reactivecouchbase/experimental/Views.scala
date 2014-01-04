c9b58b5aac5fdcf5ad1d6badae8ae88d009d304d
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-01-04 21:13:38 +0100

    Expose raw docs and typed docs

--- driver/src/main/scala/org/reactivecouchbase/experimental/Views.scala
+++ driver/src/main/scala/org/reactivecouchbase/experimental/Views.scala
@@ -11,6 +11,17 @@ case class Meta(id: String, rev: String, expiration: Long, flags: Long)
 
 case class ViewRow(document: Option[JsValue], id: Option[String], key: String, value: String, meta: Meta) {
   def toTuple = (document, id, key, value, meta)
+  def withReads[A](r: Reads[A]): TypedViewRow[A] = TypedViewRow[A](document, id, key, value, meta, r)
+}
+
+case class TypedViewRow[T](document: Option[JsValue], id: Option[String], key: String, value: String, meta: Meta, reader: Reads[T]) {
+  def JsResult: JsResult[T] = document.map( doc => reader.reads(doc)).getOrElse(JsError())
+  def Instance: Option[T] = JsResult match {
+    case JsSuccess(doc, _) => Some(doc)
+    case JsError(errors) => None
+  }
+  def toTuple = (document, id, key, value, meta)
+  def withReads[A](r: Reads[A]): TypedViewRow[A] = TypedViewRow[A](document, id, key, value, meta, r)
 }
 
 object Views {
@@ -25,7 +36,7 @@ object Views {
 
   private[reactivecouchbase] val client: AsyncHttpClient = new AsyncHttpClient(config)
 
-  private[experimental] def viewQuery(view: View, query: Query)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[JsArray] = {
+  private[experimental] def internalViewQuery(view: View, query: Query)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[JsArray] = {
     val url = s"http://${bucket.hosts.head}:8092${view.getURI}${query.toString}&include_docs=${query.willIncludeDocs()}"
     val promise = Promise[String]()
     client.prepareGet(url).execute(new AsyncCompletionHandler[Response]() {
@@ -44,13 +55,27 @@ object Views {
     promise.future.map(body => (Json.parse(body) \ "rows").as[JsArray])
   }
 
-  def query(docName: String, viewName: String, q: Query)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[Enumerator[ViewRow]] = {
-    bucket.view(docName, viewName).flatMap { view => query(view, q)(bucket, ec) }
+  def rawQuery(docName: String, viewName: String, q: Query)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[Enumerator[ViewRow]] = {
+    bucket.view(docName, viewName).flatMap { view => rawQuery(view, q)(bucket, ec) }
   }
 
-  def query(view: View, q: Query)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[Enumerator[ViewRow]] = {
-    viewQuery(view, q)(bucket, ec).map { array =>
-      Enumerator.enumerate(array.value).through(Enumeratee.map { slug =>
+  def query[T](docName: String, viewName: String, q: Query)(implicit bucket: CouchbaseBucket, r: Reads[T], ec: ExecutionContext): Future[Enumerator[TypedViewRow[T]]] = {
+    bucket.view(docName, viewName).flatMap { view => query(view, q)(bucket, r, ec) }
+  }
+
+  def query[T](view: View, q: Query)(implicit bucket: CouchbaseBucket, r: Reads[T], ec: ExecutionContext): Future[Enumerator[TypedViewRow[T]]] = {
+    internalQuery(view, q, { (doc, id, key, value, meta) => TypedViewRow[T](doc, id, key, value, meta, r) }, bucket, ec)
+  }
+
+  def rawQuery(view: View, q: Query)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[Enumerator[ViewRow]] = {
+    internalQuery(view, q, { (doc, id, key, value, meta) => ViewRow(doc, id, key, value, meta) }, bucket, ec)
+  }
+
+  private[experimental] def internalQuery[R](view: View, q: Query,
+        transformation: (Option[JsValue], Option[String], String, String, Meta) => R,
+        bucket: CouchbaseBucket, ec: ExecutionContext): Future[Enumerator[R]] = {
+    internalViewQuery(view, q)(bucket, ec).map { array =>
+      Enumerator.enumerate(array.value)(ec).through(Enumeratee.map[JsValue] { slug =>
         val meta = Meta(
           (slug \ "doc" \ "meta" \ "id").asOpt[String].getOrElse(""),
           (slug \ "doc" \ "meta" \ "rev").asOpt[String].getOrElse(""),
@@ -62,8 +87,8 @@ object Views {
         val id = (slug \ "id").asOpt[String]
         val doc = (slug \ "doc" \ "json").asOpt[JsValue]
         //if (q.willReduce())   // no doc and id
-        ViewRow(doc, id, key, value, meta)
-      })
-    }
+        transformation(doc, id, key, value, meta)
+      }(ec))
+    }(ec)
   }
 }
