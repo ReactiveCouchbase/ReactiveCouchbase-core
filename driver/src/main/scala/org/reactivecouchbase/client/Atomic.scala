8d57e8137244b27990858bc73fa861e66e72f942
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-01-04 14:20:48 +0100

    Rework on Atomic API. Kill actors as soon as they are useless.

--- driver/src/main/scala/org/reactivecouchbase/client/Atomic.scala
+++ driver/src/main/scala/org/reactivecouchbase/client/Atomic.scala
@@ -6,7 +6,7 @@ import net.spy.memcached.ops.OperationStatus
 import org.reactivecouchbase.client.CouchbaseFutures._
 import net.spy.memcached.CASValue
 import play.api.libs.json._
-import akka.actor.Actor
+import akka.actor._
 import akka.pattern._
 import akka.actor.Props
 import akka.util.Timeout
@@ -17,7 +17,7 @@ import akka.pattern.after
 import scala.util.control.ControlThrowable
 import org.reactivecouchbase.CouchbaseExpiration._
 
-case class AtomicRequest[T](key: String, operation: T => T, bucket: CouchbaseBucket, atomic: Atomic, r: Reads[T], w: Writes[T], ec: ExecutionContext, numberTry: Int)
+case class AtomicRequest[T](key: String, operation: T => Future[T], bucket: CouchbaseBucket, atomic: Atomic, r: Reads[T], w: Writes[T], ec: ExecutionContext, numberTry: Int)
 
 case class AtomicSucess[T](key: String)
 case class AtomicError[T](request: AtomicRequest[T], message: String) extends ControlThrowable
@@ -55,27 +55,42 @@ class AtomicActor[T] extends Actor {
             // get current object
             val cv = ar.r.reads(Json.parse(cas.getValue.toString)).get
             // apply transformation and get new object
-            val nv = ar.operation(cv)
-            // write new object to couchbase and unlock :-)
-            // TODO : use asyncCAS method, better io management
-            val res = ar.bucket.couchbaseClient.cas(ar.key, cas.getCas, ar.w.writes(nv).toString)
-              // reply to sender it's OK
-            res match {
-              case CASResponse.OK => sen ! Future.successful(AtomicSucess[T](ar.key))
-              case CASResponse.NOT_FOUND => sen ! Future.failed(throw new AtomicNoKeyFoundError[T](ar))
-              case CASResponse.EXISTS => {
-                // something REALLY weird append during the locking time. But anyway, we can retry :-)
-                bb.driver.logger.info("Couchbase lock WEIRD error : desync of CAS value. Retry anyway")
-                val ar2 = AtomicRequest(ar.key, ar.operation, ar.bucket, ar.atomic, ar.r, ar.w, ar.ec, ar.numberTry + 1)
-                val atomic_actor = bb.driver.system().actorOf(AtomicActor.props[T])
-                for (
-                  tr <- (atomic_actor ? ar2)
-                ) yield (sen ! tr)
+            //val nv = ar.operation(cv)
+            ar.operation(cv).map { nv =>
+              // write new object to couchbase and unlock :-)
+              // TODO : use asyncCAS method, better io management
+              val res = ar.bucket.couchbaseClient.cas(ar.key, cas.getCas, ar.w.writes(nv).toString)
+                // reply to sender it's OK
+              res match {
+                case CASResponse.OK => {
+                  sen ! Future.successful(AtomicSucess[T](ar.key))
+                  self ! PoisonPill
+                }
+                case CASResponse.NOT_FOUND => {
+                  sen ! Future.failed(throw new AtomicNoKeyFoundError[T](ar))
+                  self ! PoisonPill
+                }
+                case CASResponse.EXISTS => {
+                  // something REALLY weird append during the locking time. But anyway, we can retry :-)
+                  bb.driver.logger.warn("Couchbase lock WEIRD error : desync of CAS value. Retry anyway")
+                  val ar2 = AtomicRequest(ar.key, ar.operation, ar.bucket, ar.atomic, ar.r, ar.w, ar.ec, ar.numberTry + 1)
+                  val atomic_actor = bb.driver.system().actorOf(AtomicActor.props[T])
+                  for (
+                    tr <- (atomic_actor ? ar2)
+                  ) yield (sen ! tr)
+                  self ! PoisonPill
+                }
+                case _ => {
+                  sen ! Future.failed(throw new AtomicError[T](ar, res.name))
+                  self ! PoisonPill
+                }
               }
-              case _ => sen ! Future.failed(throw new AtomicError[T](ar, res.name))
             }
           }
-          case Failure(ex: OperationStatusErrorNotFound) => sen ! Future.failed(new AtomicNoKeyFoundError[T](ar))
+          case Failure(ex: OperationStatusErrorNotFound) => {
+            sen ! Future.failed(new AtomicNoKeyFoundError[T](ar))
+            self ! PoisonPill
+          }
           case r => {
             // Too bad, the object is not locked and some else is working on it...
             // build a new atomic request
@@ -89,15 +104,18 @@ class AtomicActor[T] extends Actor {
               tr <- (atomic_actor ? d)
             // send result :-)
             ) yield (sen ! tr)
+            self ! PoisonPill
           }
         }
       } else {
         sen ! Future.failed(throw new AtomicTooMuchTryError[T](ar))
+        self ! PoisonPill
       }
     }
     case _ => {
       logger.map(_.error("An atomic actor get a message, but not a atomic request, it's weird ! "))
       sender ! Future.failed(throw new AtomicWeirdError())
+      self ! PoisonPill
     }
   }
 }
@@ -116,12 +134,16 @@ trait Atomic {
     waitForOperationStatus(bucket.couchbaseClient.asyncUnlock(key, casId), bucket, ec)
   }
 
-  def atomicUpdate[T](key: String, operation: T => T)(implicit bucket: CouchbaseBucket, ec: ExecutionContext, r: Reads[T], w: Writes[T]): Future[Any] = {
+  def atomicallyUpdate[T](key: String, operation: T => Future[T])(implicit bucket: CouchbaseBucket, ec: ExecutionContext, r: Reads[T], w: Writes[T]): Future[T] = {
     implicit val timeout = Timeout(8 minutes)
     val ar = AtomicRequest[T](key, operation, bucket, this, r, w, ec, 1)
     val atomic_actor = bucket.driver.system().actorOf(AtomicActor.props[T])
     atomic_actor ! LoggerHolder(bucket.driver.logger)
-    (atomic_actor.ask(ar))
+    (atomic_actor.ask(ar)).map(_.asInstanceOf[T]) // I know it's ugly
+  }
+
+  def atomicUpdate[T](key: String, operation: T => T)(implicit bucket: CouchbaseBucket, ec: ExecutionContext, r: Reads[T], w: Writes[T]): Future[T] = {
+    atomicallyUpdate(key, (arg: T) => Future(operation(arg)))(bucket, ec, r, w) 
   }
 
 }
