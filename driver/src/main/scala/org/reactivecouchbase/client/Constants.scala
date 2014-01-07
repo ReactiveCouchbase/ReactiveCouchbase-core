f1ca1af3ec936a61bc15a4a31815e2e32459a663
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-01-07 16:08:32 +0100

    Doc for constants

--- driver/src/main/scala/org/reactivecouchbase/client/Constants.scala
+++ driver/src/main/scala/org/reactivecouchbase/client/Constants.scala
@@ -2,8 +2,23 @@ package org.reactivecouchbase.client
 
 import net.spy.memcached.{ReplicateTo, PersistTo}
 
+/**
+ * Some constants
+ */
 object Constants {
+
+  /**
+   * Infinity persistence
+   */
   val expiration: Int = -1
+
+  /**
+   * Standard PersistTo
+   */
   implicit val defaultPersistTo: PersistTo = PersistTo.ZERO
+
+  /**
+   * Standard ReplicateTo
+   */
   implicit val defaultReplicateTo: ReplicateTo = ReplicateTo.ZERO
 }
