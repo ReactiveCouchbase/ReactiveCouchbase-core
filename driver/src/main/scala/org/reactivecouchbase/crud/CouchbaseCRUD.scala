d316eb7be7586986c4d444d05beae7fc3ec3643d
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-01-07 16:35:41 +0100

    Documentation for CRUD API

--- driver/src/main/scala/org/reactivecouchbase/crud/CouchbaseCRUD.scala
+++ driver/src/main/scala/org/reactivecouchbase/crud/CouchbaseCRUD.scala
@@ -8,18 +8,43 @@ import com.couchbase.client.protocol.views.{Query, View}
 import play.api.libs.iteratee.{Enumeratee, Iteratee, Enumerator}
 import org.reactivecouchbase.client.{ReactiveCouchbaseException, TypedRow}
 
+/**
+ *
+ * Util trait to avoid implementing basic CRUD operations
+ *
+ * @tparam T type of the doc
+ */
 trait ReactiveCRUD[T] {
 
   import org.reactivecouchbase.CouchbaseRWImplicits._
-  
+
+  /**
+   * @return ExecutionContext for async processing
+   */
   implicit def ctx: ExecutionContext// = implicitly[ExecutionContext]
 
+  /**
+   * @return the bucket used
+   */
   def bucket: CouchbaseBucket
-  
+
+  /**
+   * @return the JSON format for T
+   */
   def format: Format[T]
-  
+
+  /**
+   * @return field name of the doc used as Id
+   */
   def idKey: String = "_id"
 
+  /**
+   *
+   * Insert a new document in the bucket
+   *
+   * @param t
+   * @return
+   */
   def insert(t: T): Future[String] = {
     val id: String = UUID.randomUUID().toString
     val json = format.writes(t).as[JsObject]
@@ -35,18 +60,48 @@ trait ReactiveCRUD[T] {
     }
   }
 
+  /**
+   *
+   * Fetch a document
+   *
+   * @param id the key of the document
+   * @return maybe the document and its key if found
+   */
   def get(id: String): Future[Option[(T, String)]] = {
     bucket.get[T]( id )(format, ctx).map( _.map( v => ( v, id ) ) )(ctx)
   }
 
+  /**
+   *
+   * Delete a document
+   *
+   * @param id key of the document
+   * @return nothing useful
+   */
   def delete(id: String): Future[Unit] = {
     bucket.delete(id)(ctx).map(_ => ())
   }
 
+  /**
+   *
+   * Update a document
+   *
+   * @param id key of the document
+   * @param t the new value for the key
+   * @return  nothing useful
+   */
   def update(id: String, t: T): Future[Unit] = {
     bucket.replace(id, t)(format, ctx).map(_ => ())
   }
 
+  /**
+   *
+   * Partially update the content of a document
+   *
+   * @param id the key of the document
+   * @param upd the partial update json object
+   * @return nothing useful
+   */
   def updatePartial(id: String, upd: JsObject): Future[Unit] = {
     get(id).flatMap { opt =>
       opt.map { t =>
@@ -57,10 +112,26 @@ trait ReactiveCRUD[T] {
     }
   }
 
+  /**
+   *
+   * Insert a stream of documents
+   *
+   * @param elems the stream of documents
+   * @return the number of documents inserted
+   */
   def batchInsert(elems: Enumerator[T]): Future[Int] = {
     elems(Iteratee.foldM[T, Int](0)( (s, t) => insert(t).map(_ => s + 1))).flatMap(_.run)
   }
 
+  /**
+   *
+   * Search for documents
+   *
+   * @param sel the query
+   * @param limit the size of the returned collection
+   * @param skip to skip some documents
+   * @return sequence of results
+   */
   def find(sel: (View, Query), limit: Int = 0, skip: Int = 0): Future[Seq[(T, String)]] = {
     var query = sel._2
     if (limit != 0) query = query.setLimit(limit)
@@ -79,6 +150,15 @@ trait ReactiveCRUD[T] {
     }
   }
 
+  /**
+   *
+   * Search for documents (result as stream)
+   *
+   * @param sel the query
+   * @param limit the size of the returned collection
+   * @param skip to skip some documents
+   * @return stream of results
+   */
   def findStream(sel: (View, Query), skip: Int = 0, pageSize: Int = 0): Enumerator[Iterator[(T, String)]] = {
     var query = sel._2
     if (skip != 0) query = query.setSkip(skip)
@@ -98,6 +178,13 @@ trait ReactiveCRUD[T] {
     Enumerator.flatten(futureEnumerator)
   }
 
+  /**
+   *
+   * Delete multiple documents
+   *
+   * @param sel query to find documents to delete
+   * @return nothing useful
+   */
   def batchDelete(sel: (View, Query)): Future[Unit] = {
     val extract = { tr: TypedRow[JsObject] => tr.id.get }
     bucket.search[JsObject](sel._1)(sel._2)(CouchbaseRWImplicits.documentAsJsObjectReader, ctx).enumerate.map { enumerator =>
@@ -105,6 +192,14 @@ trait ReactiveCRUD[T] {
     }.map(_ => ())
   }
 
+  /**
+   *
+   * Update multiple documents
+   *
+   * @param sel query to find documents to update
+   * @param upd partial update of documents
+   * @return
+   */
   def batchUpdate(sel: (View, Query), upd: JsObject): Future[Unit] = {
     bucket.search[T](sel._1)(sel._2)(format, ctx).enumerate.map { enumerator =>
       bucket.replaceStream(enumerator.through(Enumeratee.map { t =>
@@ -114,6 +209,14 @@ trait ReactiveCRUD[T] {
     }
   }
 
+  /**
+   *
+   * Fetch a view
+   *
+   * @param docName design doc name
+   * @param viewName the name of the view to fetch
+   * @return the fetched view
+   */
   private def view(docName: String, viewName: String): Future[View] = {
     bucket.view(docName, viewName)(ctx)
   }
