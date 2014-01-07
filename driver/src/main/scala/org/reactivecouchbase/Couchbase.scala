db477a2b65ff0ae7554485dbd9f759ffcef0a086
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-01-07 16:01:04 +0100

    Add some doc to API

--- driver/src/main/scala/org/reactivecouchbase/Couchbase.scala
+++ driver/src/main/scala/org/reactivecouchbase/Couchbase.scala
@@ -11,6 +11,20 @@ import org.reactivecouchbase.client._
 import com.typesafe.config.{Config, ConfigFactory}
 import com.ning.http.client.{AsyncHttpClient, AsyncHttpClientConfig}
 
+/**
+ *
+ * Representation of a Couchbase Bucket
+ *
+ * @param cbDriver ReactiveCouchbase driver associated with this bucket
+ * @param client actual Java driver
+ * @param hosts list of hosts
+ * @param port port of the server
+ * @param base base URL
+ * @param bucket name of the bucket
+ * @param user user for the bucket
+ * @param pass password for the bucket
+ * @param timeout connection timeout
+ */
 class CouchbaseBucket( private[reactivecouchbase] val cbDriver: ReactiveCouchbaseDriver,
                        private[reactivecouchbase] val client: Option[CouchbaseClient],
                        val hosts: List[String],
@@ -21,6 +35,12 @@ class CouchbaseBucket( private[reactivecouchbase] val cbDriver: ReactiveCouchbas
                        val pass: String,
                        val timeout: Long) extends BucketAPI {
 
+  /**
+   *
+   * Connect the current Java Driver to the Couchbase server
+   *
+   * @return the connected CouchbaseBucket
+   */
   def connect(): CouchbaseBucket = {
     val uris = ArrayBuffer(hosts.map { h => URI.create(s"http://$h:$port/$base") }: _*)
     val cfb = new CouchbaseConnectionFactoryBuilder()
@@ -38,51 +58,123 @@ class CouchbaseBucket( private[reactivecouchbase] val cbDriver: ReactiveCouchbas
     new CouchbaseBucket(cbDriver, Some(client), hosts, port, base, bucket, user, pass, timeout)
   }
 
+  /**
+   *
+   * Disconnect the current Java Driver from the Couchbase server
+   *
+   * @return the disconnected CouchbaseBucket
+   */
   def disconnect(): CouchbaseBucket = {
     client.map(_.shutdown(timeout, TimeUnit.SECONDS))
     new CouchbaseBucket(cbDriver, None, hosts, port, base, bucket, user, pass, timeout)
   }
 
+  /**
+   * @return the actual Java Driver
+   */
   def couchbaseClient: CouchbaseClient = {
     client.getOrElse(throw new ReactiveCouchbaseException(s"Error with bucket ${bucket}", s"Bucket '${bucket}' is not defined or client is not connected"))
   }
 
+  /**
+   * @return the actual ReactiveCouchbaseDriver
+   */
   def driver: ReactiveCouchbaseDriver = cbDriver
 
+  /**
+   * Check if Futures from Java Driver are failed. If so, fails scala Future
+   */
   private[reactivecouchbase] val checkFutures = cbDriver.configuration.getBoolean("couchbase.driver.checkfuture").getOrElse(false)
+
+  /**
+   * Fails Json processing if Json structure is wrong
+   */
   private[reactivecouchbase] val jsonStrictValidation = cbDriver.configuration.getBoolean("couchbase.json.validate").getOrElse(true)
+
+  /**
+   * Fail Scala futures if OperationStatus is failed
+   */
   private[reactivecouchbase] val failWithOpStatus = cbDriver.configuration.getBoolean("couchbase.failfutures").getOrElse(false)
+
+  /**
+   * Timeout
+   */
   private[reactivecouchbase] val ecTimeout: Long = cbDriver.configuration.getLong("couchbase.actorctx.timeout").getOrElse(1000L)
+
+  /**
+   * Use experimental Query API instead of Java Drivers one
+   */
   private[reactivecouchbase] val useExperimentalQueries: Boolean = cbDriver.configuration.getBoolean("couchbase.experimental.queries").getOrElse(false)
+
+  /**
+   * Optional N1QL host
+   */
   private[reactivecouchbase] val N1QLHost = driver.configuration.getString("couchbase.n1ql.host")
+
+  /**
+   * Optional N1QL port
+   */
   private[reactivecouchbase] val N1QLPort = driver.configuration.getString("couchbase.n1ql.port")
 
-
+  /**
+   * Configuration for HTTP client
+   */
   private[reactivecouchbase] val config: AsyncHttpClientConfig = new AsyncHttpClientConfig.Builder()
-    .setAllowPoolingConnection(true)
-    .setCompressionEnabled(true)
-    .setRequestTimeoutInMs(600000)
-    .setIdleConnectionInPoolTimeoutInMs(600000)
-    .setIdleConnectionTimeoutInMs(600000)
+    .setAllowPoolingConnection(cbDriver.configuration.getBoolean("couchbase.http.pool").getOrElse(true))
+    .setCompressionEnabled(cbDriver.configuration.getBoolean("couchbase.http.compression").getOrElse(true))
+    .setRequestTimeoutInMs(cbDriver.configuration.getInt("couchbase.http.reqtimeout").getOrElse(60000))
+    .setIdleConnectionInPoolTimeoutInMs(cbDriver.configuration.getInt("couchbase.http.idlepool").getOrElse(60000))
+    .setIdleConnectionTimeoutInMs(cbDriver.configuration.getInt("couchbase.http.idleconnection").getOrElse(60000))
     .build()
 
+  /**
+   * The HTTP client dedicated for this bucket (used for view queries and N1QL support)
+   */
   private[reactivecouchbase] val httpClient: AsyncHttpClient = new AsyncHttpClient(config)
 }
 
+/**
+ * Driver to access a Couchbase server
+ */
 class ReactiveCouchbaseDriver(as: ActorSystem, config: Configuration, log: LoggerLike) {
 
+  /**
+   * All the buckets managed by this driver
+   */
   val buckets = new ConcurrentHashMap[String, CouchbaseBucket]
 
+  /**
+   * Config for each bucket
+   */
   val bucketsConfig: Map[String, Config] = config.getObjectList("couchbase.buckets")
     .getOrElse(throw new RuntimeException("Can't find any bucket in conf !!!"))
     .map(_.toConfig)
     .map(b => (b.getString("bucket"), b))
     .toMap
 
+  /**
+   * @return actor system for this driver
+   */
   def system(): ActorSystem = as
+
+  /**
+   * @return ExecutionContext for this driver
+   */
   def executor(): ExecutionContext = as.dispatcher
+
+  /**
+   * @return Akka Scheduler for this driver
+   */
   def scheduler(): Scheduler = as.scheduler
+
+  /**
+   * @return Configuration for this driver
+   */
   def configuration: Configuration = config
+
+  /**
+   * @return Logger wrapper for this driver
+   */
   def logger: LoggerLike = log
 
   def bucket(hosts: List[String], port: String, base: String, bucket: String, user: String, pass: String, timeout: Long): CouchbaseBucket = {
@@ -108,30 +200,96 @@ class ReactiveCouchbaseDriver(as: ActorSystem, config: Configuration, log: Logge
     buckets.get(name)
   }
 
+  /**
+   *
+   * Provide a Bucket as Capped bucket.
+   * A capped bucket is a special bucket where insertion order of documents is kept
+   *
+   * @param name name of the Couchbase bucket
+   * @param max max number of element in the capped bucket
+   * @param reaper activate reaper to delete elements when the capped bucket is full (ie. bigger than max)
+   * @return a capped bucket
+   */
   def cappedBucket(name: String, max: Int, reaper: Boolean): CappedBucket = CappedBucket(bucket(name), bucket(name).driver.executor(), max, reaper)
+
+  /**
+   *
+   * Provide a Bucket as Capped bucket.
+   * A capped bucket is a special bucket where insertion order of documents is kept
+   *
+   * @param name name of the Couchbase bucket
+   * @param ec the ExecutionContext for async processing
+   * @param max max number of element in the capped bucket
+   * @param reaper activate reaper to delete elements when the capped bucket is full (ie. bigger than max)
+   * @return a capped bucket
+   */
   def cappedBucket(name: String, ec: ExecutionContext, max: Int, reaper: Boolean): CappedBucket = CappedBucket(bucket(name), ec, max, reaper)
 
+  /**
+   *
+   * Create a N1QL query
+   *
+   * @param query the actual query written in N1QL query language
+   * @return a new N1QL query
+   */
   def N1QL(query: String): N1QLQuery = {
     CouchbaseN1QL.N1QL(query)(this.buckets.head._2)
   }
 
+  /**
+   * Shutdown this driver
+   */
   def shutdown() {
     buckets.foreach(t => t._2.disconnect())
+    system().shutdown()
   }
 }
 
+/**
+ * Companion object to build ReactiveCouchbaseDriver
+ */
 object ReactiveCouchbaseDriver {
 
+  /**
+   *
+   * Create the default ActorSystem if not provided
+   *
+   * @return the default actor system
+   */
   private def defaultSystem = {
     import com.typesafe.config.ConfigFactory
     val config = ConfigFactory.load()
     ActorSystem("ReactiveCouchbaseSystem", config.getConfig("couchbase.actorctx"))
   }
 
+  /**
+   * @return a new ReactiveCouchbaseDriver with default actor system and configuration
+   */
   def apply(): ReactiveCouchbaseDriver = new ReactiveCouchbaseDriver(defaultSystem, new Configuration(ConfigFactory.load()), StandaloneLogger)
+
+  /**
+   * @param system a custom ActorSystem provided by the user
+   * @return a new ReactiveCouchbaseDriver with default configuration
+   */
   def apply(system: ActorSystem): ReactiveCouchbaseDriver = new ReactiveCouchbaseDriver(system, new Configuration(ConfigFactory.load()), StandaloneLogger)
+
+  /**
+   * @param system a custom ActorSystem provided by the user
+   * @param config a custom configuration provided by the user
+   * @return a new ReactiveCouchbaseDriver
+   */
   def apply(system: ActorSystem, config: Configuration): ReactiveCouchbaseDriver = new ReactiveCouchbaseDriver(system, config, StandaloneLogger)
+
+  /**
+   * @param system a custom ActorSystem provided by the user
+   * @param config a custom configuration provided by the user
+   * @param logger a custom logger wrapper provided by the user
+   * @return a new ReactiveCouchbaseDriver
+   */
   def apply(system: ActorSystem, config: Configuration, logger: LoggerLike): ReactiveCouchbaseDriver = new ReactiveCouchbaseDriver(system, config, logger)
 }
 
+/**
+ * Main API for Couchbase generic operations.
+ */
 object Couchbase extends Read with Write with Delete with Counters with Queries with JavaApi with Atomic {}
