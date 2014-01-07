7dbff3c0e28a2d4ac0869a4430b50314cc25c497
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-01-07 16:08:42 +0100

    Doc for counter operations

--- driver/src/main/scala/org/reactivecouchbase/client/Counters.scala
+++ driver/src/main/scala/org/reactivecouchbase/client/Counters.scala
@@ -5,39 +5,119 @@ import scala.concurrent.{Future, ExecutionContext}
 import net.spy.memcached.ops.OperationStatus
 import org.reactivecouchbase.client.CouchbaseFutures._
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Counter Operations
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Trait for number operations
+ */
 trait Counters {
 
+  /**
+   *
+   * Increment an Int
+   *
+   * @param key key of the Int value
+   * @param by increment of the value
+   * @param bucket bucket to use
+   * @param ec ExecutionContext for async processing
+   * @return
+   */
   def incr(key: String, by: Int)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[OperationStatus] = {
     waitForOperationStatus( bucket.couchbaseClient.asyncIncr(key, by), bucket, ec )
   }
 
+  /**
+   *
+   * Increment a Long
+   *
+   * @param key key of the Long value
+   * @param by the value to increment
+   * @param bucket bucket to use
+   * @param ec ExecutionContext for async processing
+   * @return
+   */
   def incr(key: String, by: Long)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[OperationStatus] = {
     waitForOperationStatus( bucket.couchbaseClient.asyncIncr(key, by), bucket, ec )
   }
 
+  /**
+   *
+   * Decrement an Int
+   *
+   * @param key key of the Int value
+   * @param by the value to decrement
+   * @param bucket bucket to use
+   * @param ec ExecutionContext for async processing
+   * @return
+   */
   def decr(key: String, by: Int)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[OperationStatus] = {
     waitForOperationStatus( bucket.couchbaseClient.asyncDecr(key, by), bucket, ec )
   }
 
+  /**
+   *
+   * Decrement a Long
+   *
+   * @param key key of the Long value
+   * @param by the value to decrement
+   * @param bucket bucket to use
+   * @param ec ExecutionContext for async processing
+   * @return
+   */
   def decr(key: String, by: Long)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[OperationStatus] = {
     waitForOperationStatus( bucket.couchbaseClient.asyncDecr(key, by), bucket, ec )
   }
 
+  /**
+   *
+   * Increment and get an Int
+   *
+   * @param key key of the Int value
+   * @param by the value to increment
+   * @param bucket bucket to use
+   * @param ec ExecutionContext for async processing
+   * @return
+   */
   def incrAndGet(key: String, by: Int)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[Int] = {
     Future[Long]( bucket.couchbaseClient.incr(key, by) )(ec).map(_.toInt)
   }
 
+  /**
+   *
+   * Increment and get a Long
+   *
+   * @param key key of the Long value
+   * @param by the value to increment
+   * @param bucket bucket to use
+   * @param ec ExecutionContext for async processing
+   * @return
+   */
   def incrAndGet(key: String, by: Long)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[Long] = {
     Future[Long]( bucket.couchbaseClient.incr(key, by) )(ec)
   }
 
+  /**
+   *
+   * Decrement and get an Int
+   *
+   * @param key key of the Int value
+   * @param by the value to decrement
+   * @param bucket bucket to use
+   * @param ec ExecutionContext for async processing
+   * @return
+   */
   def decrAndGet(key: String, by: Int)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[Int] = {
     Future[Long]( bucket.couchbaseClient.decr(key, by) )(ec).map(_.toInt)
   }
 
+  /**
+   *
+   * Decrement and get a Long
+   *
+   * @param key key of the Long value
+   * @param by the value to decrement
+   * @param bucket bucket to use
+   * @param ec ExecutionContext for async processing
+   * @return
+   */
   def decrAndGet(key: String, by: Long)(implicit bucket: CouchbaseBucket, ec: ExecutionContext): Future[Long] = {
     Future[Long]( bucket.couchbaseClient.decr(key, by) )(ec)
   }
