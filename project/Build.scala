5ae200821905576f2a8c81eaf88b8c64dca35045
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-01-19 14:44:43 +0100

    Prepare maven central publication

--- project/Build.scala
+++ project/Build.scala
@@ -9,11 +9,19 @@ object ApplicationBuild extends Build {
   val appScalaBinaryVersion = "2.10"
   val appScalaCrossVersions = Seq("2.10.2")
 
-  val local: Project.Initialize[Option[sbt.Resolver]] = version { (version: String) =>
+  val local: Def.Initialize[Option[sbt.Resolver]] = version { (version: String) =>
     val localPublishRepo = "./repository"
     if(version.trim.endsWith("SNAPSHOT"))
       Some(Resolver.file("snapshots", new File(localPublishRepo + "/snapshots")))
     else Some(Resolver.file("releases", new File(localPublishRepo + "/releases")))
+  }
+
+  val nexusPublish = version { v =>
+    val nexus = "https://oss.sonatype.org/"
+    if (v.trim.endsWith("SNAPSHOT"))
+      Some("snapshots" at nexus + "content/repositories/snapshots")
+    else
+      Some("releases"  at nexus + "service/local/staging/deploy/maven2")
   }
 
   lazy val baseSettings = Defaults.defaultSettings ++ Seq(
@@ -49,6 +57,26 @@ object ApplicationBuild extends Build {
       publishTo <<= local,
       publishMavenStyle := true,
       publishArtifact in Test := false,
-      pomIncludeRepository := { _ => false }
+      pomIncludeRepository := { _ => false },
+      pomExtra := (
+        <url>http://reactivecouchbase.org</url>
+          <licenses>
+            <license>
+              <name>Apache 2</name>
+              <url>http://www.apache.org/licenses/LICENSE-2.0</url>
+              <distribution>repo</distribution>
+            </license>
+          </licenses>
+          <scm>
+            <url>git@github.com:ReactiveCouchbase/ReactiveCouchbase-core.git</url>
+            <connection>scm:git:git@github.com:ReactiveCouchbase/ReactiveCouchbase-core.git</connection>
+          </scm>
+          <developers>
+            <developer>
+              <id>mathieu.ancelin</id>
+              <name>Mathieu ANCELIN</name>
+              <url>https://github.com/mathieuancelin</url>
+            </developer>
+          </developers>)
     )
 }
