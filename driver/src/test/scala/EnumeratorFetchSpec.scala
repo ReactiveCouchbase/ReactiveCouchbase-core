5122a940834145ac131d8c8d59522e8af380aea7
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-05-08 13:46:28 +0200

    Add test for empty key seq

--- driver/src/test/scala/EnumeratorFetchSpec.scala
+++ driver/src/test/scala/EnumeratorFetchSpec.scala
@@ -2,6 +2,7 @@ import org.reactivecouchbase.{CouchbaseRWImplicits, ReactiveCouchbaseDriver}
 import org.reactivecouchbase.CouchbaseRWImplicits.documentAsJsObjectReader
 import org.reactivecouchbase.CouchbaseRWImplicits.jsObjectToDocumentWriter
 import org.specs2.mutable.{Tags, Specification}
+import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.{JsArray, JsObject, Json}
 import scala.concurrent.{ExecutionContext, Await}
 
@@ -19,8 +20,20 @@ You need to start a Couchbase server with a 'default' bucket on standard port to
 
   "ReactiveCouchbase Read API" should {
 
-    "not be able to fetch vales" in {
-      val list = Await.result(bucketDefault.fetchValues[JsObject](Seq[String]("key1", "key2")).toList, timeout)
+    "not be able to fetch non existing values" in {
+      val list = Await.result(bucketDefault.fetchValues[JsObject](Seq[String]("aspoQdxjSUFKiJ5Vzes56Serpjbv78Xz0SDBdbataH26kOJ8I4spAjrJoLmk2Cca", "E46T7OpyQM8c2du63L0GYtNeyA8e1rRMG9WNynEliCMrHZgQOgT4HEgEzvJWynQo")).toList, timeout)
+      list.size shouldEqual 0
+      success
+    }
+
+    "be able to fetch no values" in {
+      val list = Await.result(bucketDefault.fetchValues[JsObject](Seq[String]()).toList, timeout)
+      list.size shouldEqual 0
+      success
+    }
+
+    "be able to fetch no values from enumerator" in {
+      val list = Await.result(bucketDefault.fetchValues[JsObject](Enumerator.empty[String]).toList, timeout)
       list.size shouldEqual 0
       success
     }
